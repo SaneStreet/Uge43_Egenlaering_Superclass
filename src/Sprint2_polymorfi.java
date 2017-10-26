d9dd3ac1e07eb71bf1ba85135e5735d2a5f6acd0
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-10-26 12:10:00 +0200

    reee

--- src/Sprint2_polymorfi.java
+++ src/Sprint2_polymorfi.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Sprint2_polymorfi {
     public static void main(String[] args) {
         //1.Vis et eksempel på at du overrider Object klassens toString() metode i din kode.
@@ -19,6 +21,27 @@ public class Sprint2_polymorfi {
         int trash = (int) 2.205; //her runder den ned
         System.out.println(trash);
 
+        //3b. equals()
+        String some = "some string";
+        String other = "some string";
+        //Den spørg i teorien bare om some er det samme som equal, hvor den viser true.
+        boolean equal = some.equals(other);
+        //Hvis man gør sådan her printer den false.
+        boolean equal2 = some == other;
+
+        //4. Arraylist
+        //Først laves en arraylist med typen inden i <>.
+        ArrayList<Integer> testArInt = new ArrayList<Integer>();
+        testArInt.add(2); // her adder man en array til tablet med værdien 2, så array[0] = 2
+        testArInt.add(14); // array[1] = 14
+        testArInt.size(); // Viser size
+        ArrayList<String> testArStr = new ArrayList<String>();
+        testArStr.add("Mads");
+        testArStr.clear();
+
+        //5. Hvordan kan du forhindre, at en klasse kan udvides med subclasses? Vis i din kode.
+        //Man kan gøre det hele til private eks. public class Sprint2_polymorfi{}
+        //Man kan heller ikke extend en final class eller final method.
 
 
     }
@@ -27,9 +50,9 @@ public class Sprint2_polymorfi {
         System.out.println("This Poly is great");
     }
 
-    //3a.Hvad gør instanceof? Vis eksempel i din kode.
+    //3a. Hvad gør instanceof? Vis eksempel i din kode.
     //den checker om det er en ting (evt. int, eller en klasse som f.eks. hund)
-    //Her checker den eksemple vist om
+    //Her checker den eksemple vist om Number(et tal bruger skriver) param, er en Double eller Int
     public void doSomething(Number param) {
         if( param instanceof Double) {
             System.out.println("param is a Double");
@@ -37,13 +60,10 @@ public class Sprint2_polymorfi {
         else if( param instanceof Integer) {
             System.out.println("param is an Integer");
         }
-
-        if( param instanceof Comparable) {
-            //subclasses of Number like Double etc. implement Comparable
-            //other subclasses might not -> you could pass Number instances that don't implement that interface
-            System.out.println("param is comparable");
-        }
     }
+
+
+
 
 
 
